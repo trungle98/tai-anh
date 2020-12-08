ca706902b4a55ade1615ca20757b67e5a0c88249
trungle98hn <trungle98hn@gmail.com>
2020-12-09 03:16:37 +0700

    add default page

--- testFlask.py
+++ testFlask.py
@@ -2,7 +2,10 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 app = Flask(__name__)
- 
+@app.route('/')
+def welcome():
+    return "Welcome to Wisdom robotics"
+
 @app.route('/map')
 def hello_world():
     return render_template('hello.html', name="map")
