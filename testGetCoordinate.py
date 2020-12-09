9eccbdb56deeb8fea922cf5734865f15464a8a5e
trungle98hn <trungle98hn@gmail.com>
2020-12-10 01:46:25 +0700

    getCoordinate

--- testGetCoordinate.py
+++ testGetCoordinate.py
@@ -1,1 +1,12 @@
-print("ok")
+from bs4 import BeautifulSoup
+
+import requests
+import urllib.request
+import shutil
+
+url = "https://rubikscode.net/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+aas = soup.find_all("a", class_='entry-featured-image-url')
