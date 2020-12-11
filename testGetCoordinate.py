4b6acfcf4e714b90233f226a6652e8287357cf85
trungle98 <37513675+trungle98@users.noreply.github.com>
2020-12-11 16:37:35 +0700

    Update testGetCoordinate.py

--- testGetCoordinate.py
+++ testGetCoordinate.py
@@ -4,16 +4,38 @@ import requests
 import urllib.request
 import shutil
 import time
+from serial import Serial
+
+port = '/dev/ttyUSB1'
+for i in range(4):
+    try:
+        arduino = Serial(port,19200,timeout=5)
+    except:
+        print("error")
 
 url = "https://tai-anh.herokuapp.com/getAutoMove"
+urlDefault = "https://tai-anh.herokuapp.com/setAutoMove?x=0&y=0"
 #url = "http://127.0.0.1:5000/getAutoMove"
-for i in range(100):
+preLng = 0
+preLat = 0
+def sendData(data):
+    arduino.flush()
+    temp = str(data)
+    arduino.write(temp.encode("utf-8"))
+    response = requests.get(urlDefault)
+    print(data)
+while True:
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     lng = soup.find_all("h1", {"id":"lng"})
     lat = soup.find_all("h1", {"id":"lat"})
-    print("lng: " +str(lng)[14:-6])
-    print("lat: " +str(lat)[14:-6])
-    time.sleep(1)
+    lng = str(lng)[14:-6]
+    lat = str(lat)[14:-6]
+    if (lng != str(preLng) and lat != str(preLat)) or (int(lng) != 0 and int(lat) != 0):
+        sendData(lat+","+lng+"\n")
+        print((lng != preLng and lat != preLat))
+        preLat = lat
+        preLng = lng
+    time.sleep(3)
